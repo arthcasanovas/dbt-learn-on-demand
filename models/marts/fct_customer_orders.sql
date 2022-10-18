dc7702bf4bd5847fc24035921fc0065b34677f04
artur.casanovas.snowflake@gmail.com <artur.casanovas.snowflake@gmail.com>
2022-10-18 14:21:36 +0000

    final CTE

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -46,46 +46,53 @@ completed_payments as (
 
 ),
 
+-- Final CTE 
 
+final as (
 
-customer_orders 
-    as (select customers.id as customer_id,
-        min(orders.order_date) as first_order_date,
-        max(orders.order_date) as most_recent_order_date,
-        count(orders.id) as number_of_orders
-    from customers  
-    left join orders on orders.user_id = customers.id 
-    group by 1)
+  select
+    order_id,
+    customer_id,
+    order_placed_at,
+    order_status,
+    total_amount_paid,
+    payment_finalized_date,
+    customer_first_name,
+    customer_last_name,
 
-select
-    paid_orders.*,
-    row_number() over (order by paid_orders.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
-    ---new vs returning customer
-    case 
-    when (
-        rank() over (
-            partition by customer_id
-            order by order_placed_at, order_id
-        ) = 1
+    -- sales transaction sequence
+    row_number() over (order by order_id) as transaction_seq,
+
+    -- customer sales sequence
+    row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
+
+    -- new vs returning customer
+    case  
+      when (
+      rank() over (
+      partition by customer_id
+      order by order_placed_at, order_id
+      ) = 1
     ) then 'new'
     else 'return' end as nvsr,
-    x.clv_bad as customer_lifetime_value,
+
+    -- customer lifetime value
+    sum(total_amount_paid) over (
+      partition by customer_id
+      order by order_placed_at
+      ) as customer_lifetime_value,
+
     -- first day of sale
-    first_value(paid_orders.order_placed_at) over (
-        partition by paid_orders.customer_id
-        order by paid_orders.order_placed_at
-    ) as fdos
-    from paid_orders 
-    left join customer_orders as c using (customer_id)
-    left outer join 
-    (
-            select
-            paid_orders.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-        from paid_orders 
-        left join paid_orders t2 on paid_orders.customer_id = t2.customer_id and paid_orders.order_id >= t2.order_id
-        group by 1
-        order by paid_orders.order_id
-    ) x on x.order_id = paid_orders.order_id
-    order by order_id
+    first_value(order_placed_at) over (
+      partition by customer_id
+      order by order_placed_at
+      ) as fdos
+
+    from paid_orders
+		
+)
+
+-- Simple Select Statement
+
+select * from final
+order by order_id
